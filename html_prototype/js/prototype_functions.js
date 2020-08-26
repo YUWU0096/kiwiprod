69063ada1ab44a3a3d9d6c770933dce2d3eca423
lcmiracle <cliang96@gmail.com>
2020-08-26 19:29:48 +0800

    How It Works Page
    Added How it Works Page.
    Need to realize page transistion with "analysis button".

--- html_prototype/js/prototype_functions.js
+++ html_prototype/js/prototype_functions.js
@@ -7,13 +7,18 @@ function click_nav_btn(element) {
             location.href="prototype_home.html";
             break;
         case "how-btn":
-            alert("How it works page dummy response.");
+            location.href="prototype_how_it_works.html";
             break;
         case "advice-btn":
-            alert("Career advice page dummy response.");
+            location.href="prototype_advice.html";
             break;
         case "contact-btn":
             location.href="prototype_contact.html";
+            break;
+        case "start-analyse-btn":
+            location.href="prototype_info_page.html";
+            break;
+        default:
             break;
     }
 }
